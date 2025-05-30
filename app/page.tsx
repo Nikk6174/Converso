309555b5304883163ba2da201a381335f61519fc
Nikk6174 <nikk.6174@gmail.com>
2025-05-30 19:59:24 +0530

    Add CompanionCard, CompanionList, and CTA components; update main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,51 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">welcome to my app</h1>
-       <Button>
-        Click Me now
-       </Button>
-    </div>
+    <main>
+      <h1 >Popular companion</h1>
+       
+       <section className="home-section">
+        <CompanionCard
+          id = "123"
+          name = "neura the brainy explorer"
+          topic = "neural networks"
+          subject = " science"
+          duration = {45}
+          colour = "#afda6e"
+          />
+
+          <CompanionCard
+            id="456"
+            name="Countsy the Number Wizard"
+            topic="Derivatives & Integrals"
+            subject="maths"
+            duration={30}
+            colour="#e5d0ff"
+          />
+
+          <CompanionCard
+            id="789"
+            name="Verba the Vocabulary Builder"
+            topic="Language"
+            subject="English Literature"
+            duration={45}
+            colour="#ffda6e"
+          />
+        
+       </section>
+
+        <section className="home-section">
+          <CompanionList/>
+          <CTA/> 
+
+        </section>
+
+    </main>
   )
 }
 
---
+++ components/CTA.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// call to action component
+const CTA = () => {
+  return (
+    <div>CTA</div>
+  )
+}
+
+export default CTA
