70d53625b8cfaef2a2b9de0e39a8da34ac4232f2
Nikk6174 <nikk.6174@gmail.com>
2025-05-31 17:46:49 +0530

    feat: integrate Clerk for authentication and update Navbar with sign-in functionality

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,5 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+  return  <main className="flex justify-center h-screen items-center"><SignIn /></main>
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,47 @@
 import React from 'react'
-import  Link  from 'next/link';
-import  Image  from 'next/image';
+import Link from 'next/link';
+import Image from 'next/image';
 import NavItems from './NavItems';
 
-const Navbar = () => {
-  return (
-    <nav className='navbar'>
-        <Link href='/'>
-            <div className="flex items-center gap-2.5 cursor-pointer">
+import { type Metadata } from 'next'
+import {
+    ClerkProvider,
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from '@clerk/nextjs'
 
-                <Image src = "/images/logo.svg" alt = "logo" width = {46} height = {44}/>
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <Link href='/'>
+                <div className="flex items-center gap-2.5 cursor-pointer">
+
+                    <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
+
+                </div>
+            </Link>
+
+            <div className="flex items-center gap-8">
+                <NavItems />
+
+                <SignedOut>
+                        <SignInButton>
+                            <button className='btn-signin'>
+                                Sign In
+                            </button>
+                        </SignInButton>
+                </SignedOut>
+
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
 
             </div>
-        </Link>
-
-        <div className="flex items-center gap-8">
-            <NavItems/>
-            <p>Sign In</p>
-
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
