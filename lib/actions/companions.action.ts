bc14e15b1b2291fdfb3b4d0d97e5659fcfec9f7b
Nikk6174 <nikk.6174@gmail.com>
2025-05-31 22:09:12 +0530

    fix: correct casing in table name and improve import formatting

--- lib/actions/companions.action.ts
+++ lib/actions/companions.action.ts
@@ -1,14 +1,14 @@
-'use server';
+"use server";
 
-import { auth } from "@clerk/nextjs/server";
-import { createSupabaseClient } from "../supabase";
+import {auth} from "@clerk/nextjs/server";
+import {createSupabaseClient} from "../supabase";
 
 export const createCompanion = async(formData: CreateCompanion)=>{
     const {userId: author} = await auth();
     const supabase = createSupabaseClient();
 
     const {data, error} = await supabase
-        .from("companions")
+        .from("Companions")
         .insert({...formData, author})
         .select();
 
